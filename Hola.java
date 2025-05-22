b7ec600dee8000afb0b6fe9ddd9c0503aa3d1951
alisonnguerraag <andrea.guerra0104@gmail.com>
2025-05-21 18:18:41 -0600

    Update Hola.java

--- Hola.java
+++ Hola.java
@@ -1,5 +1,5 @@
 public class Hola {
   public static void main(String[] args) {
-    System.out.println("Hola, este es el cambio numero 1");
+    System.out.println("Buen dia, este es el cambio numero 2 al proyecto java.");
   }
 }
